ce28fbbbb858d6818f19d99f37efb7cb6c235fd2
Yeo byung hoon <ybhsos@ntmobile.kr>
2021-04-08 01:02:39 +0900

    3,4번 항목 테스트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import R003_ImportComponent from "./R003_ImportComponent";
+import R004_LifecycleEx from "./R004_LifecycleEx";
 
 function App(){
   return (
       <div>
           <h1>Start React 200!</h1>
-          <p>CSS 적용하기</p>
+          <R003_ImportComponent></R003_ImportComponent>
+          <R004_LifecycleEx></R004_LifecycleEx>
       </div>
   );
 }
